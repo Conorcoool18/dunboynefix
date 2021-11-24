61e787e397738f109187b32f360b80af28a627ef
blackout2007 <94967985+blackout2007@users.noreply.github.com>
2021-11-24 12:23:29 +0000

    Create breakAndtest.js

---
+++ breakAndtest.js
@@ -0,0 +1,30 @@
+// This class is due to return information to car owners
+// it is based on the fuel they input
+// this should return a message to the user based on the method that is called
+
+class Fuel {
+    constructor(fuelName, price, fuelType) {
+        this.name = fuelName;
+        this.origin = price;
+        this.furlType = fuelType;
+    }
+
+
+    get fuel Price() {
+      
+        // here needs to be some code to tell the price of this fuel
+    
+
+    get Fuel Details() { 
+        
+        // here needs to be some code to tell the all the details of this fuel
+    }
+
+
+    
+    
+    
+   get Recommendation() {}
+
+
+   // here needs to be some code that returns a recommendation on the car owner going forward
